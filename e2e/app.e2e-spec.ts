9eba2ed875ca17392208d2fe370573a1bb06ee75
angular-cli <angular-cli@angular.io>
2017-02-18 08:41:31 +0200

    chore: initial commit from angular-cli                           _                      _ _    __ _ _ __   __ _ _   _| | __ _ _ __       ___| (_)   / _  |  _ \ / _  | | | | |/ _  |  __|____ / __| | |  | (_| | | | | (_| | |_| | | (_| | | |_____| (__| | |   \____|_| |_|\__  |\____|_|\____|_|        \___|_|_|               |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { StockportfolioPage } from './app.po';
+
+describe('stockportfolio App', function() {
+  let page: StockportfolioPage;
+
+  beforeEach(() => {
+    page = new StockportfolioPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});
